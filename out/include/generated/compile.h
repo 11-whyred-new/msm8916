9b6fa99a6f675653d36d7601ddbb58ab960fe6a8
rrnanairo <alandika009@gmail.com>
2021-11-05 15:05:16 +0700

    fix
    Change-Id: I931cd4d2f7ef1121a52c121185d6ece5fe75dd75

--- out/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm64"
-#define UTS_VERSION "#1 SMP PREEMPT Thu Nov 4 21:15:09 WIB 2021"
-#define LINUX_COMPILE_BY "ubuntu"
-#define LINUX_COMPILE_HOST "ip-172-31-38-208"
-#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
